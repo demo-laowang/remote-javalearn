1fcddbc8dfff29af0632a8aa9b188068843dc339
demo-laowang <wangleigang0318@163.com>
2025-08-15 17:24:07 +0800

    Update Main.java
    从github上进行修改

--- javalearn-day01/src/com/myjavalearn/myfirstjavaproject/Main.java
+++ javalearn-day01/src/com/myjavalearn/myfirstjavaproject/Main.java
@@ -2,6 +2,6 @@ package com.myjavalearn.myfirstjavaproject;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello world!222");
+        System.out.println("hello world!333");
     }
 }
